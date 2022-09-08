bf5cbcdfbd6d73046ab90cf4d4cbb0872636fd90
Johan Dahl <johan_dal@hotmail.com>
2022-09-08 12:47:24 +0200

    Builds the UI of the App

--- lib/main.dart
+++ lib/main.dart
@@ -11,105 +11,136 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Steg 1',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        title: const Text("TIG 333 TODO"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: _todoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SecondView()));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+Widget _todoList() {
+  var todo = [
+    "Diska",
+    "Städa",
+    "Plugga",
+    "Laga mat",
+    "Arbeta",
+    "Gå ut med hunden",
+    "Spela boll",
+    "Läsa tidningen",
+    "Springa",
+    "Dricka öl",
+  ];
+
+  var list = List.generate(todo.length, (index) => todo[index]);
+
+  return ListView.builder(
+    padding: const EdgeInsets.only(
+      top: 20,
+      bottom: kFloatingActionButtonMargin + 55,
+    ),
+    itemCount: todo.length,
+    itemBuilder: (context, index) {
+      return Column(
+        children: <Widget>[
+          _listItem(list[index]),
+          const Divider(height: 20, thickness: 3),
+        ],
+      );
+    },
+  );
+}
+
+Widget _listItem(text) {
+  return ListTile(
+    leading: Checkbox(
+      value: false,
+      onChanged: (val) {},
+    ),
+    title: Text(
+      text,
+      style: const TextStyle(fontSize: 25),
+    ),
+    trailing: IconButton(
+      icon: const Icon(Icons.clear),
+      onPressed: () {},
+    ),
+  );
+}
+
+class SecondView extends StatelessWidget {
+  const SecondView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("TIG333 TODO"),
+      ),
+      body: Column(
+        children: [
+          _todoInputField(),
+          Container(height: 24),
+          _buttonRow(),
+        ],
+      ),
+    );
+  }
+
+  Widget _todoInputField() {
+    return Container(
+      margin: const EdgeInsets.only(left: 30, right: 30, top: 40),
+      child: const TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "What are you going to do?",
+          labelStyle: TextStyle(fontSize: 20),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buttonRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+        const Text(
+          "ADD",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
